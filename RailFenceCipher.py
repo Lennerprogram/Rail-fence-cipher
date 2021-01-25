703c5e8970201712c054f1b2348aae4ade054cfa
Lenner Coutinho <coutinho.lenner@gmail.com>
2021-01-25 15:39:42 -0300

    added RailFenceCipher

---
+++ RailFenceCipher.py
@@ -0,0 +1,50 @@
+def RailFence(rail, string):
+    #variable creation
+    a = []
+    togetherlist = []
+    line = 0
+    column = 0
+    increment = True
+
+    #transform string to list
+    string_split = list(string)
+
+    #matrix creation
+    for c in range(rail):
+        a.append( ['|'] * len(string))
+
+    #placement of values
+    while True:
+        if rail - line == 1:
+            increment = False
+        elif line == 0:
+            increment = True
+
+        a[line] [column] = string_split[column]
+        
+        if increment:
+            line += 1
+        else:
+            line -= 1
+
+        if column == len(string) - 1:
+            break
+
+        column += 1
+
+    #output
+    for i in a:
+        print(i)
+
+    #encrypted
+    for c in range(rail):
+        while a[c].count('|') > 0:
+            a[c].remove('|')
+
+    for c in range(rail):
+        togetherlist += a[c]
+
+    criptografado = ''.join(togetherlist)
+
+    print(criptografado)
+    return criptografado
